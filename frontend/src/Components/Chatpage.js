aef7642c7ca16bfdaea381f5f5ce014eea54b4fa
Sarvesh <sarveshmd11@gmail.com>
2022-04-20 23:54:35 +0530

    latest changes

--- frontend/src/Components/Chatpage.js
+++ frontend/src/Components/Chatpage.js
@@ -3,13 +3,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Chatpage = () => {
+  const [chats, setChats] = useState(null);
   const getchat = async () => {
     const { data } = await axios.get("/api/chat");
-    console.log(data);
+    // console.log(data);
+    setChats(data);
   };
   useEffect(() => {
     getchat();
   }, []);
-  return <div>Chatpage hai mera</div>;
+  //console.log(chats);
+  return (
+    <div>
+      {chats && chats.map((chat) => <div key={chat._id}>{chat.chatName}</div>)}
+    </div>
+  );
 };
 export default Chatpage;
